4b45d5032122ea1f35479efc9f982026a2a06ca2
JamesFletcher00 <123896424+JamesFletcher00@users.noreply.github.com>
2025-03-14 02:14:21 +0000

    spawn point switch case
    broken

--- Assets/GameLogicController.cs
+++ Assets/GameLogicController.cs
@@ -6,37 +6,16 @@ public class GameLogicController : MonoBehaviour
     private GameObject RedChip;
     public GameObject yellowChip;
     private GameObject YellowChip;
+    public Transform A7Spawn;
+    public Transform B7Spawn;
+    public Transform C7Spawn;
+    public Transform D7Spawn;
+    public Transform E7Spawn;
+    public Transform F7Spawn;
+    public Transform G7Spawn;
     public int turn = 1;
     public string playerTurn;
     public bool roundActive;
-
-    void RoundChecker()
-    {
-
-    }
-
-    void ChipSpawner()
-    {
-
-    }
-    
-    void RedSpawn()
-    {
-        if (roundActive == true)
-        {
-            turn ++;
-            RedChip = Instantiate(redChip);
-        }
-    }
-    void YellowSpawn()
-    {
-        if (roundActive == true)
-        {
-            turn --;
-            YellowChip = Instantiate(yellowChip);
-            roundActive = false;
-        }
-    }
 
 
     void Update()
@@ -49,20 +28,56 @@ public class GameLogicController : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log("" + hit.collider.gameObject.name);
+                Transform spawnPoint = SpawnOnGrid(hit.collider.tag);
+
+                if (spawnPoint != null)
+                {
+                    SpawnItem(spawnPoint.position);
+                }
             }
         }
-        if(Input.GetKeyDown(KeyCode.Space) && roundActive == false)
-        {
-            roundActive = true;
-        }
-        if(Input.GetKeyDown(KeyCode.Mouse0) && turn == 1)
-        {
-            RedSpawn();
-        }
-        else if(Input.GetKeyDown(KeyCode.Mouse1) && turn == 2)
-        {
-            YellowSpawn();
-        }
-        Debug.Log(turn);
     }
+    void RoundChecker()
+    {
+
+    }
+
+    void SpawnOnGrid(string tag)
+    {
+        switch(tag)
+        {
+            case "A":
+                return A7spawn; break;
+            case "B":
+                return B7spawn; break;
+            case "C":
+                return C7spawn; break;
+            case "D":
+                return D7spawn; break;
+            case "E":
+                return E7spawn; break;
+            case "F":
+                return F7spawn; break;
+            case G:
+                return G7spawn; break;
+            default:
+                return null;
+
+        }
+    }
+    void ChipSpawner()
+    {
+        if (roundActive == true)
+            if (turn == 1)
+            {
+                turn ++;
+                RedChip = Instantiate(redChip, position, Quarternion.identity);
+            }
+            if (turn == 2)
+            {
+                turn --;
+                YellowChip = Instantiate(yellowChip, position, Quarternion.identity);
+            }
+    }
+
 }
